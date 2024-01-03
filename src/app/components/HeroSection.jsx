b7f540b8a61f58b0dc3d19d3f63d4f46a6c388cc
Daniel Alves <daniel.xcoders@gmail.com>
2024-01-03 19:00:21 -0300

    fix: adjust mobile font size

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+          <h1 className="text-white mb-4 text-4xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Olá! Eu sou{" "}
             </span>
@@ -32,8 +32,8 @@ const HeroSection = () => {
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Ex-Médica Veterinária apaixonada por Web Design 💜.
+          <p className="text-[#ADB7BE] text-base sm:text-sm mb-6 lg:text-xl">
+            Ex-Médica Veterinária apaixonada por Web Design 💜
           </p>
           <div>
             <Link
